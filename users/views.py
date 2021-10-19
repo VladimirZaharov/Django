254a7466a0f4f18d929bf0c6d7ad896d1f34f048
Vladimir <vovazakharov2@gmail.com>
2021-10-19 17:33:05 +0300

    done

--- users/views.py
+++ users/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import auth
 
-from users.forms import UserLoginForm
+
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
+
     context = {
         'title': 'GeekShop - Авторизация',
         'form': form
@@ -12,5 +28,14 @@ def login(request):
     return render(request, 'users/login.html', context)
 
 def registration(request):
-    context = {'title': 'GeekShop - Регистрация'}
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegistrationForm()
+    context = {'title': 'GeekShop - Регистрация', 'form': form}
     return render(request, 'users/registration.html', context)
