3bd0ba413a899801718028cd773897f47d0fda02
Vladimir <vovazakharov2@gmail.com>
2021-10-19 12:22:43 +0300

    add user login form

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from users.forms import UserLoginForm
+
 # Create your views here.
 def login(request):
-    context = {'title': 'GeekShop - Авторизация'}
+    form = UserLoginForm()
+    context = {
+        'title': 'GeekShop - Авторизация',
+        'form': form
+    }
     return render(request, 'users/login.html', context)
 
 def registration(request):
