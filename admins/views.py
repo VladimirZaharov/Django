3a49a7bd83583d32735e72a83506ddd6009ef00e
Vladimir <vovazakharov2@gmail.com>
2021-10-30 16:02:07 +0300

    added admin

--- admins/views.py
+++ admins/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
+@user_passes_test(lambda u: u.is_staff)
 def index(request):
     context = {
         'title': 'GeekShop - Админ Панель'
@@ -11,6 +13,7 @@ def index(request):
     return render(request, 'admins/index.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_users_create(request):
     if request.method == 'POST':
         form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
@@ -26,6 +29,7 @@ def admin_users_create(request):
         return render(request, 'admins/admin-users-create.html', context)
 
 
+@user_passes_test(lambda u: u.is_staff)
 def admin_users(request):
     context = {
         'title': 'GeekShop - Пользователи',
@@ -34,12 +38,26 @@ def admin_users(request):
     return render(request, 'admins/admin-user-read.html', context)
 
 
-def admin_users_update(request):
+@user_passes_test(lambda u: u.is_staff)
+def admin_users_update(request, id):
+    selected_user = User.objects.get(id=id)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=selected_user, files=request.FILES, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=selected_user)
     context = {
-        'title': 'GeekShop - Обновление пользователя'
+        'title': 'GeekShop - Обновление пользователя',
+        'form': form,
+        'selected_user': selected_user
     }
     return render(request, 'admins/admin-users-update-delete.html', context)
 
 
-def admin_users_delete(request):
-    pass
+@user_passes_test(lambda u: u.is_staff)
+def admin_users_delete(request, id):
+    user = User.objects.get(id=id)
+    user.safe_delete()
+    return HttpResponseRedirect(reverse('admins:admin_users'))
--- baskets/admin.py
+++ baskets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from baskets.models import Basket
 # Register your models here.
+
+class BasketAdmin(admin.TabularInLine):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    extra = 0
+    readonly_fields = ('created_timestamp',)
