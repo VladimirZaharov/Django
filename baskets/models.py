89940e38a8afa9040eaa2bef6e94928b55c09541
Vladimir <vovazakharov2@gmail.com>
2021-12-04 22:39:56 +0300

    added decorator and with teg

--- baskets/models.py
+++ baskets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.functional import cached_property
 
 from users.models import User
 from products.models import Product
@@ -16,7 +17,7 @@ from products.models import Product
 class Basket(models.Model):
     # objects = BasketQuerySet.as_manager()
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
     created_timestamp = models.DateTimeField(auto_now_add=True)
@@ -28,12 +29,16 @@ class Basket(models.Model):
         return self.product.price * self.quantity
 
     def total_sum(self):
-        baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.sum() for basket in baskets)
 
     def total_quantity(self):
-        baskets = Basket.objects.filter(user=self.user)
+        baskets = self.get_items_cached
         return sum(basket.quantity for basket in baskets)
+
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
 
     @staticmethod
     def get_items(user):
