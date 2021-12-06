92c6eb610ca5339259e9d066edb15c50a4a98b8f
Vladimir <vovazakharov2@gmail.com>
2021-12-06 14:32:38 +0300

    added ajax

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from products.views import products
+from django.views.decorators.cache import cache_page
+
+from products.views import products, products_ajax
 
 app_name = 'products'
 
 urlpatterns = [
     path('',products, name='index'),
+    path('ajax/',cache_page(3600)(products_ajax)),
     path('<int:category_id>/', products, name='category'),
     path('page/<int:page>', products, name='page')
 ]
--- products/views.py
+++ products/views.py
@@ -1,14 +1,21 @@
+from django.template.loader import render_to_string
+
 from products.models import Product, ProductCategory
 
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.cache import cache_page
+from django.http import JsonResponse
+
+
 
 # Create your views here.
+@cache_page(3600)
 def index(request):
     context = {'title': 'geekshop'}
     return render(request, 'products/index.html', context)
 
-
+@cache_page(3600)
 def products(request, category_id=None, page=1):
     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
     if category_id:
@@ -24,3 +31,22 @@ def products(request, category_id=None, page=1):
         products_paginator = paginator.page(paginator.num_pages)
     context['products'] = products_paginator
     return render(request, 'products/products.html', context)
+
+def products_ajax(request, category_id=None, page=1):
+
+    if request.is_ajax():
+        context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
+        if category_id:
+            products = Product.objects.filter(category_id=category_id)
+        else:
+            products = Product.objects.all()
+        paginator = Paginator(products, 3)
+        try:
+            products_paginator = paginator.page(page)
+        except PageNotAnInteger:
+            products_paginator = paginator.page(1)
+        except EmptyPage:
+            products_paginator = paginator.page(paginator.num_pages)
+        context['products'] = products_paginator
+        result = render_to_string('products/inc_products.html', context=context, request=request)
+    return JsonResponse({'result': result})
